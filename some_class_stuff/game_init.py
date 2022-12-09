bcc5b2ca774f4eb166566886e646638d521d81e0
Dean <dean.marinov@gmx.at>
2022-12-09 12:36:21 +0100

    add_lootbox

--- some_class_stuff/game_init.py
+++ some_class_stuff/game_init.py
@@ -9,12 +9,15 @@ import random as rnd
 player = p.player()
 #init items
 sword = i.Item("Sword", 10, 2,3,0,15,0,1,0)
-player.player_add_item(sword)
 shield = i.Item("Shield", 5, 1,1,0,50,0,1,2)
+helmet = i.Item("Helmet", 4, 1, 3, 1, 5, 5, 1, 0)
+
+
+player.player_add_item(sword)
 player.player_add_item(shield)
 
-#current_item = player.player_get_item(shield)
-#current_item.item_print()
-player.player_inventory_print()
+m.Monster.monster_set_loot_bag(sword)
+m.Monster.monster_set_loot_bag(shield)
+m.Monster.monster_set_loot_bag(helmet)
 
 
--- some_class_stuff/monster.py
+++ some_class_stuff/monster.py
@@ -10,6 +10,14 @@ class Monster():
 	intelligence = level * rnd.randint(1,level *3)
 	healthpoints = strength * level
 	manapoints = intelligence * level
+	expirience_give = level * strength
 
 	def monster_add_items(self, item):
 		self.items.append(item)
+
+	def monster_set_loot_bag(self, item):
+		self.items.append(item)
+
+	def monster_get_loot_bag(self):
+		return self.items
+		
