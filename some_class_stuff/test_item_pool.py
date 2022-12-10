5584dba82ee4b360b55717c315eb4530fd3a1ed4
Dean <dean.marinov@gmx.at>
2022-12-11 00:17:33 +0100

    update file names

--- some_class_stuff/test_item_pool.py
+++ some_class_stuff/test_item_pool.py
@@ -1,14 +1,14 @@
 """TESTINT item pool and drop chance
 """
 
-import item_pool
+import item_init
 import unittest
 
 class Testing(unittest.TestCase):
     def test_count(self):
-        item_pool.item_pool_init()
-        count = len(item_pool.pool)
+        item_init.item_init_init()
+        count = len(item_init.pool)
         self.assertGreater(count, 0, print('items in pool =', count))
         
     def test_drop(self):
-        self.assertLess(item_pool.Drop_factor(), 51, print('possible chance for an item to drop =', item_pool.Drop_factor()))
+        self.assertLess(item_init.Drop_factor(), 51, print('possible chance for an item to drop =', item_init.Drop_factor()))
