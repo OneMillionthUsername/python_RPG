1f5882e2c4813676b403e0cfab00b6a355336cf5
Dean <dean.marinov@gmx.at>
2022-12-11 17:54:21 +0100

    merge item_pool into item

--- some_class_stuff/item.py
+++ some_class_stuff/item.py
@@ -11,6 +11,10 @@ class Item():
 	Levelbonus = None
 	Drop_chance = None
 	Rarity = ['normal', 'magic', 'rare', 'epic', 'unique', 'legendary']
+	weapon_names_warrior = ["Sword", "Shield", "Axe"]
+	weapon_names_magician = ["Staff", "Wand", "Papyrus"]
+	weapon_names_bowmaster = ["Bow", "Crossbow", "Slingshot"]
+	pool = []
 	
 	def __init__(self, name, price, strength, agility, intelligence, health, mana, level, chance, rarity = 0) -> None:
 		self.Name = name
@@ -40,3 +44,10 @@ class Item():
 			self.Rarity = self.Rarity[1]
 		else:
 			self.Rarity = self.Rarity[0]
+
+	def item_pool_init(self):
+		j = 0
+		for j in range(10):
+			item = Item(self.weapon_names_warrior[r.randint(0,2)], r.randint(5,15), r.randint(1,5), r.randint(1,4), r.randint(1,2), r.randint(1,10) * 10, 0, r.randint(1,5), r.randint(1,100))
+			self.pool.append(item)
+			j += 1
