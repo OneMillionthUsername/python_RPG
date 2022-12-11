686387cf3c07f667a3f46f11f62e76f3ca8955bf
Dean <dean.marinov@gmx.at>
2022-12-11 19:15:32 +0100

    check avg rarity dropchance

--- some_class_stuff/game_init.py
+++ some_class_stuff/game_init.py
@@ -7,14 +7,14 @@ def main():
 	#main
 	count = 0
 	condition = True
+	player = p.player("Dean")
 	while condition:
 		#init player
-		player = p.player("Dean")
 		#init monster
 		monster = m.Monster("Bat", "flying", 4)
 		condition = f.fight(player, monster)
 		count += 1
-	print('Number of fights for legendary item:', count)
+	print('Number of fights for this item rarity:', count)
 
 if __name__ == "__main__":
 	main()
